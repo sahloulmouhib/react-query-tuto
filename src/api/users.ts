0b9413c8a807b9db034b6f9d27ea50def4e83f1a
Mouhib Sahloul <>
2023-01-18 16:21:58 +0100

    add user interface

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,7 +1,33 @@
 import { BASE_URL } from "./posts";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+
+interface IUser {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
 
 export async function getUser(id: number) {
-  const res = await axios.get(`${BASE_URL}/users/${id}`);
+  const res = await axios.get<any, AxiosResponse<IUser>>(
+    `${BASE_URL}/users/${id}`
+  );
   return res.data;
 }
